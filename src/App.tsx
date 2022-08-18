73a9595cacc44ec4ed4cc6381545e746d5d51a3c
Jonathan Bridge <jvbridge@gmail.com>
2022-08-18 16:03:43 -0700

    Projects route and page

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Projects from './pages/Projects';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="/*" element={<NotFound />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </Router>
       <Footer />
