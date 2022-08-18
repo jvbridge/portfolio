dadc01df03257ec04d3e320e833b2c978b7be855
Jonathan Bridge <jvbridge@gmail.com>
2022-08-18 15:51:43 -0700

    made the header a navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,28 @@
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import icon from './images/icon.jpg';
+
+const iconStyle = {
+  maxHeight: 80,
+  maxWidth: 80,
+};
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={icon} alt="thought bubble icon" style={iconStyle}></img>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/">About</Nav.Link>
+            <Nav.Link href="/">Projects</Nav.Link>
+            <Nav.Link href="/">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
