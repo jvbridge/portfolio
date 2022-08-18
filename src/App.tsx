c217e4b88dd09d048822a1a1af5615100e9f6c62
Jonathan Bridge <jvbridge@gmail.com>
2022-08-18 15:57:24 -0700

    not found page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// CSS
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Pages
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <>
       <Header />
-      <div>Body</div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Router>
       <Footer />
     </>
   );
