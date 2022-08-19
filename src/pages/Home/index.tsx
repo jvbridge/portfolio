4a6e13c395ab6f20b78457e7bd2552abaefa8f3b
Jonathan Bridge <jvbridge@gmail.com>
2022-08-19 11:55:26 -0700

    including pdf types

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,15 @@
+import { Container } from 'react-bootstrap';
+import background from './assets/main_background.png';
+import resume from './assets/resume.pdf';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <div>
+        <h1>Jonathan Bridge</h1>
+        <h2>Full Stack developer</h2>
+        <a href={resume}>Download Resume</a>
+      </div>
+    </Container>
+  );
 }
