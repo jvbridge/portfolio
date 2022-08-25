89861fee411692948941788fd71708542da15166
Jonathan Bridge <jvbridge@gmail.com>
2022-08-25 13:14:36 -0700

    main home content

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,48 @@
-import { Container } from 'react-bootstrap';
 import background from './assets/main_background.png';
 import resume from './assets/resume.pdf';
 
+const mask = {
+  backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: ' auto',
+  height: '100vh',
+};
+
+const backgroundImage = {
+  backgroundImage: `url(${background})`,
+  height: '100vh',
+};
+
 export default function Home() {
   return (
-    <Container>
-      <div>
-        <h1>Jonathan Bridge</h1>
-        <h2>Full Stack developer</h2>
-        <a href={resume}>Download Resume</a>
+    <div style={backgroundImage}>
+      <div style={mask} className="">
+        <div
+          className="container d-flex align-items-center justify-content-center text-center h-100"
+          style={{
+            color: 'white',
+            backgroundColor: 'rgba(0,0,0,0)',
+          }}
+        >
+          <div className="text-white">
+            <div id="lead">
+              <div id="lead-content">
+                <h1>Jonathan Bridge</h1>
+                <h2>Full Stack developer</h2>
+              </div>
+              <a
+                className="btn btn-outline-light btn-lg m-2"
+                href={resume}
+                role="button"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Download Resume
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </Container>
+    </div>
   );
 }
